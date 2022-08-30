7bcacde8a611ed63913728510cf7a4e44f848954
xanqus <sbk98031816@gmail.com>
2022-08-31 00:03:59 +0900

    header design

--- src/layouts/Header.jsx
+++ src/layouts/Header.jsx
@@ -6,7 +6,67 @@ const Header = () => {
   const [authenticated, setAuthenticated] = useRecoilState(authenticatedState);
   return (
     <header>
-      <div>header</div>
+      <div className="h-14 fixed top-0 left-0 w-full">
+        <div className="navbar min-h-0 h-14 bg-base-100">
+          <div className="flex">
+            <label
+              className="btn btn-square btn-ghost hover:bg-transparent"
+              htmlFor="my-drawer"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                className="inline-block w-5 h-5 stroke-current"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h16M4 18h16"
+                ></path>
+              </svg>
+            </label>
+            <a className="btn btn-ghost normal-case text-xl hover:bg-transparent">
+              <i className="fi fi-brands-youtube text-red-500 mt-2"></i>
+              <div className="ml-2">MyTube</div>
+            </a>
+          </div>
+          <div className="flex-1 justify-end gap-2 ml-auto">
+            <div className="form-control mx-auto w-1/3">
+              <input
+                type="text"
+                placeholder="Search"
+                className="input input-bordered border-gray-400"
+              />
+            </div>
+            <div className="dropdown dropdown-end">
+              <label tabIndex="0" className="btn btn-ghost btn-circle avatar">
+                <div className="w-10 rounded-full">
+                  <img src="https://placeimg.com/80/80/people" />
+                </div>
+              </label>
+              <ul
+                tabIndex="0"
+                className="mt-3 p-2 shadow menu menu-compact dropdown-content bg-base-100 rounded-box w-52"
+              >
+                <li>
+                  <a className="justify-between">
+                    Profile
+                    <span className="badge">New</span>
+                  </a>
+                </li>
+                <li>
+                  <a>Settings</a>
+                </li>
+                <li>
+                  <a>Logout</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
     </header>
   );
 };
--- src/layouts/Layout.jsx
+++ src/layouts/Layout.jsx
@@ -6,7 +6,25 @@ const Layout = ({ children }) => {
   return (
     <>
       <Header />
-      <main className="mt-20">{children}</main>
+      <main className="mt-14 bg-red-500">{children}</main>
+      <div className="h-screen">
+        <div className="drawer">
+          <input id="my-drawer" type="checkbox" className="drawer-toggle" />
+          <div className="drawer-content"></div>
+          <div className="drawer-side">
+            <label htmlFor="my-drawer" className="drawer-overlay"></label>
+            <ul className="menu p-4 overflow-y-auto w-80 bg-base-100 text-base-content">
+              <li>
+                <a>Sidebar Item 1</a>
+              </li>
+              <li>
+                <a>Sidebar Item 2</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <div className="h-screen"></div>
       <Footer />
     </>
   );
