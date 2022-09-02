1c796d37254acaa679189dabc28e5cce837f5a59
xanqus <sbk98031816@gmail.com>
2022-09-02 12:25:47 +0900

    add create video modal box component

--- src/components/common/ModalBox.jsx
+++ src/components/common/ModalBox.jsx
@@ -11,8 +11,8 @@ const ModalBox = ({ active, setActive }) => {
         checked={active}
         onChange={() => {}}
       />
-      <label htmlFor="my-modal-4" className="modal cursor-pointer">
-        <label className="flex flex-col modal-box relative max-w-full w-240 h-208 rounded-md p-0">
+      <div htmlFor="my-modal-4" className="modal">
+        <div className="flex flex-col modal-box relative max-w-full w-240 h-208 rounded-md p-0">
           <div className="flex border-b h-14">
             <div className="flex justify-center items-center ml-5 text-lg">
               동영상 업로드
@@ -29,15 +29,30 @@ const ModalBox = ({ active, setActive }) => {
               </label>
             </div>
           </div>
-          <div className="flex h-96 bg-red-500"></div>
-          <div className="h-auto flex-grow bg-green-500">
-            <div></div>
+          <div className="flex justify-center items-end h-96">
+            <label
+              htmlFor="video-upload"
+              className="flex justify-center items-center w-36 h-36 rounded-full cursor-pointer mb-10 bg-gray-100"
+            >
+              <i className="fi fi-rr-folder-upload text-8xl text-gray-600" />
+            </label>
+          </div>
+          <div className="flex flex-col items-center h-auto flex-grow">
+            <input id="video-upload" type="file" w-24 hidden />
             <div>
-              <input type="file" w-24 />
+              <div>동영상 파일을 드래그 앤 드롭하여 업로드</div>
+              <div>동영상을 게시하기 전에는 비공개로 설정됩니다.</div>
+            </div>
+            <div>
+              <div className="flex justify-center items-center w-24 h-10 mt-8 bg-blue-500 text-white cursor-pointer">
+                <label className="cursor-pointer" htmlFor="video-upload">
+                  파일 선택
+                </label>
+              </div>
             </div>
           </div>
-        </label>
-      </label>
+        </div>
+      </div>
     </>
   );
 };
