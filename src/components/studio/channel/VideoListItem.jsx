250ad6339a37a1131c423188e1130ba9b3178ad0
xanqus <sbk98031816@gmail.com>
2022-09-16 10:47:37 +0900

    editor page bugfix

--- src/components/studio/channel/VideoListItem.jsx
+++ src/components/studio/channel/VideoListItem.jsx
@@ -1,12 +1,12 @@
-import React from 'react';
-import { useNavigate } from 'react-router-dom';
-import { useRecoilState } from 'recoil';
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import { useRecoilState } from "recoil";
 import {
   currentlySelectedVideoState,
   isEditingState,
   modalActiveState,
-} from '../../../recoil';
-import { formatDate } from '../../../utils';
+} from "../../../recoil";
+import { formatDate } from "../../../utils";
 
 const VideoListItem = ({ video }) => {
   const [active, setActive] = useRecoilState(modalActiveState);
@@ -18,28 +18,29 @@ const VideoListItem = ({ video }) => {
   return (
     video && (
       <tr>
-        <th className='border-y'>
+        <th className="border-y">
           <input
-            type='checkbox'
-            checked='checked'
-            className='rounded-none checkbox checkbox-sm focus:ring-0'
+            type="checkbox"
+            checked="checked"
+            className="rounded-none checkbox checkbox-sm focus:ring-0"
             onChange={() => {}}
           />
         </th>
-        <td className='border-y'>
-          <div className='flex'>
-            <div className='w-36'>
-              <img src={video.thumbnailUrl} alt='thumnail' />
+        <td className="border-y">
+          <div className="flex">
+            <div className="w-36">
+              <img src={video.thumbnailUrl} alt="thumnail" />
             </div>
-            <div className='flex flex-col ml-6'>
+            <div className="flex flex-col ml-6">
               <div
-                className='cursor-pointer hover:underline'
+                className="cursor-pointer hover:underline"
                 onClick={() => {
                   if (video.isTemp) {
                     setSelectedVideo(video);
                     setActive(true);
                     setIsEditing(true);
                   } else {
+                    setSelectedVideo(video);
                     navigate(`/studio/video/${video.videoId}/edit`, {
                       state: { video },
                     });
@@ -52,19 +53,19 @@ const VideoListItem = ({ video }) => {
             </div>
           </div>
         </td>
-        <td className='border-y'>
-          {video.isTemp ? '초안' : video.isPublic ? '공개' : '미공개'}
+        <td className="border-y">
+          {video.isTemp ? "초안" : video.isPublic ? "공개" : "미공개"}
         </td>
-        <td className='border-y'>없음</td>
-        <td className='border-y'>
-          <div className='flex flex-col'>
+        <td className="border-y">없음</td>
+        <td className="border-y">
+          <div className="flex flex-col">
             <div>{formatDate(video.regDate)}</div>
             <div>게시날짜</div>
           </div>
         </td>
-        <td className='border-y'>{video.views}</td>
-        <td className='border-y'>0</td>
-        <td className='border-y'>{video.likeCount}</td>
+        <td className="border-y">{video.views}</td>
+        <td className="border-y">0</td>
+        <td className="border-y">{video.likeCount}</td>
       </tr>
     )
   );
