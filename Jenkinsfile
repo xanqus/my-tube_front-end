e81c7f2116d5faba9e76b405a09bb35a97fe058e
xanqus <sbk98031816@gmail.com>
2022-12-16 10:22:06 +0900

    add ci/cd config

---
+++ Jenkinsfile
@@ -0,0 +1,54 @@
+pipeline {
+    agent any
+
+    // agent {
+    //     docker {
+    //         image 'node:16.13.1-alpine'
+    //     }
+    // }
+
+    // agent {
+    //     label 'main-host'
+    // }
+
+    // tools {
+    //     docker "latest"
+    // }
+    stages {
+        stage('connect test'){
+            steps{
+                sh """
+                echo connect
+                """
+            }
+        }
+         stage('Build') {
+            agent {
+                docker {
+                    image 'node:18.12.1-alpine'
+                }
+            }
+            steps {
+                sh 'npm install'
+                sh 'npm run build'
+            }
+        }
+
+        stage('Docker build') {
+            agent any
+            steps {
+                sh 'docker build -t my-tube-frontend:latest .'
+            }
+        }
+
+        stage('Docker run') {
+            agent any
+            steps {
+                sh 'docker ps -f my-tube-frontend -q | xargs --no-run-if-empty docker container stop'
+                sh 'docker container ls -a -f name=my-tube-frontend -q | xargs -r docker container rm'
+                sh 'docker images --no-trunc --all --quiet --filter="dangling=true" | xargs --no-run-if-empty docker rmi'
+                sh 'docker run -d --name my-tube-frontend-dev -p 8090:80 my-tube-frontend:latest'
+            }
+        }
+    }
+}
--- src/components/videoSearch/SearchedVideoItem.jsx
+++ src/components/videoSearch/SearchedVideoItem.jsx
@@ -1,7 +1,7 @@
-import React from 'react';
-import { useRef } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { formatDate } from '../../utils';
+import React from "react";
+import { useRef } from "react";
+import { useNavigate } from "react-router-dom";
+import { formatDate } from "../../utils";
 
 const SearchedVideoItem = ({ video }) => {
   const navigate = useNavigate();
@@ -9,7 +9,7 @@ const SearchedVideoItem = ({ video }) => {
 
   return (
     <div
-      className='flex gap-3 cursor-pointer max-w-4xl w-full'
+      className="flex gap-3 cursor-pointer max-w-4xl w-full"
       onClick={() => {
         navigate(`/watch?id=${video.id}`);
       }}
@@ -37,41 +37,41 @@ const SearchedVideoItem = ({ video }) => {
         videoRef.current.currentTime = 0;
       }}
     >
-      <div className='w-80'>
+      <div className="w-80">
         <video
           src={video.video_url}
           poster={video.thumbnail_url}
-          alt=''
-          className='rounded-lg'
+          alt=""
+          className="rounded-lg"
           ref={videoRef}
         />
       </div>
-      <div className='flex flex-col gap-2 flex-grow'>
+      <div className="flex flex-col gap-2 flex-grow">
         <div>{video?.title}</div>
-        <div className='flex gap-3'>
+        <div className="flex gap-3">
           <div>조회수 {video?.views}회</div>
           <div>{formatDate(video.reg_date)}</div>
         </div>
         <div
-          className='flex gap-2'
+          className="flex gap-2"
           onClick={(e) => {
-            if (e.currentTarget != e.target) {
+            if (e.currentTarget !== e.target) {
               e.stopPropagation();
               navigate(`/channel/${video.channel_id}`);
             }
           }}
         >
-          <div className='w-5 mt-1'>
-            <img src={video?.channel_profile_image_url} alt='' />
+          <div className="w-5 mt-1">
+            <img src={video?.channel_profile_image_url} alt="" />
           </div>
           <div>{video?.channel_name}</div>
         </div>
         <div>{video?.description}</div>
 
         <div
-          className='w-full h-full cursor-default'
+          className="w-full h-full cursor-default"
           onClick={(e) => {
-            if (e.currentTarget == e.target) {
+            if (e.currentTarget === e.target) {
               e.stopPropagation();
             }
           }}
